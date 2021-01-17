8353332af93faff0eb1c729d94bae807fff59845
sk <seonk94@naver.com>
2021-01-17 21:09:08 +0900

    feat: openlayers overlay

---
+++ src/components/overlay/Overlay.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode, useContext, useEffect, useRef, useState } from 'react';
+import MapContext from '../map/MapContext';
+import { Map, Overlay } from 'ol';
+import { OverlayTrigger, Popover, Tooltip } from 'react-bootstrap';
+// eslint-disable-next-line import/named
+import Icon, { IconType } from '@icon-park/react/es/all';
+import { ListItem } from 'src/types';
+interface Props {
+  item: ListItem;
+}
+const OLOverlay = ({ item }: Props) => {
+  const { map } = useContext(MapContext);
+  
+  useEffect(() => {
+    if (!map) return;
+    const overlay = new Overlay({
+      element : document.getElementById('ol-popup') as HTMLElement
+    });
+    (map as Map).addOverlay(overlay);
+    overlay.setPosition(item.coordinate);
+
+    // (map as Map).on('singleclick', (e) => {
+    //   const coordinate = e.coordinate;
+    //   console.log(coordinate);
+    //   const hdms = toStringHDMS(toLonLat(coordinate));
+    //   overlay.setPosition(coordinate);
+    // });
+
+  }, [map]);
+  return (
+    <OverlayTrigger placement="top" overlay={
+      <Tooltip id="tooltip">
+        {item.title}
+      </Tooltip>
+    }>
+      <Icon id="ol-popup" type={item.iconType as IconType} theme="filled" />
+    </OverlayTrigger>
+  );
+};
+export default OLOverlay;
