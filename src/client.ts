77c6bdc142a70d03a21f5dc8556cd8cbc25d1302
sk <seonk94@naver.com>
2021-02-08 21:27:15 +0900

    fix: env var name

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+
 export const client = new ApolloClient({
   uri : 'https://graphql.fauna.com/graphql',
   headers : {
-    authorization : `Bearer ${process.env.DB_KEY}`
+    authorization : `Bearer ${process.env.REACT_APP_DB_KEY}`
   },
   cache : new InMemoryCache()
 });
