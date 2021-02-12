2d01fec661baa3bc35dd5ecd6b4cbad00b65a03b
sk <seonk94@naver.com>
2021-02-12 15:49:21 +0900

    fix: marker 엉뚱한데 렌더링되던점 수정

--- src/components/overlay/MarkerOverlay.tsx
+++ src/components/overlay/MarkerOverlay.tsx
@@ -1,40 +1,33 @@
-import React, { useContext, useEffect } from 'react';
+import React, { createRef, useContext, useEffect } from 'react';
 import MapContext from '../map/MapContext';
 import { Map, Overlay } from 'ol';
 import { Icon, Popup } from 'semantic-ui-react';
 import { Record } from 'src/lib/graphql/record';
-import { toLonLat } from 'ol/proj';
 interface Props {
   item: Record;
 }
 const MarkerOverlay = ({ item }: Props) => {
   const { map } = useContext(MapContext);
-  
+  const iconRef = createRef<HTMLDivElement>();
   useEffect(() => {
     if (!map) return;
     const overlay = new Overlay({
-      element : document.getElementById(String(item._id)) as HTMLElement
+      element : iconRef.current as HTMLElement
     });
     (map as Map).addOverlay(overlay);
-    // console.log(item.coordinate);
     overlay.setPosition(item.coordinate);
 
-    // (map as Map).on('singleclick', (e) => {
-    //   const coordinate = e.coordinate;
-    //   console.log(coordinate);
-    //   const hdms = toStringHDMS(toLonLat(coordinate));
-    //   overlay.setPosition(coordinate);
-    // });
 
   }, [map]);
-
-  // useEffect(() => {
-  //   if (!map) return;
-  //   console.log('setposition');
-  //   overlay.setPosition(toLonLat(item.coordinate));
-  // }, [item]);
   return (
-    <Popup content={item.title} size="tiny" position="right center" trigger={<Icon id={item._id} name={item.iconType} />} />
+    <Popup 
+      content={item.title} size="tiny" position="right center" 
+      trigger={
+        <div ref={iconRef}>
+          <Icon id={item._id} name={item.iconType} />
+        </div>
+      } 
+    />
   );
 };
 export default MarkerOverlay;
