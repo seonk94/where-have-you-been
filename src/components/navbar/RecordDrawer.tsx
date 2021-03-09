a0cf5c6be477ee133e4c9ce16226901d231a71a2
sk <seonk94@naver.com>
2021-03-09 22:55:51 +0900

    feat: record data list 출력

--- src/components/navbar/RecordDrawer.tsx
+++ src/components/navbar/RecordDrawer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import { Divider, Grid, IconButton, List, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Divider, Grid, IconButton, List, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
 import { useMainTemplateState } from 'src/lib/provider/RecordProvider';
 import { MoreVert } from '@material-ui/icons';
 import styled from 'styled-components';
@@ -29,6 +29,7 @@ const useStyles = makeStyles({
 
 const ListItem = styled.div`
   display: block;
+  padding: 4px;
 `;
 
 const TitleRow = styled.div`
@@ -37,8 +38,15 @@ const TitleRow = styled.div`
   width: 100%;
 `;
 
+const DateRow =styled.div`
+display: flex;
+align-items: center;
+width: 100%;
+`;
+
 function RecordDrawer({ toggleDrawer, drawer } : Props) {
   const classes = useStyles();
+  const { data } = useMainTemplateState();
 
   const list = () => (
     <div
@@ -46,7 +54,32 @@ function RecordDrawer({ toggleDrawer, drawer } : Props) {
       role="presentation"
     >
       <List className={classes.list}>
-        
+        {
+          data.map((record, index) => <>
+            <ListItem key={record._id}>
+              <TitleRow>
+                <Typography variant="h6">
+                  {record.title}
+                </Typography>
+                <div className={classes.spacer}/>
+                <IconButton size="small" edge="start" color="inherit" aria-label="menu">
+                  <MoreVert />
+                </IconButton>
+              </TitleRow>
+              <DateRow>
+                <Typography variant="caption">
+                  {record.date}
+                </Typography>
+              </DateRow>
+              <Grid item>
+                <Typography variant="subtitle2">
+                  {record.content}
+                </Typography>
+              </Grid>
+            </ListItem>
+            {index !== data.length -1 && <Divider/>}
+          </>)
+        }
       </List>
     </div>
   );
